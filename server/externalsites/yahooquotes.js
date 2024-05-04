5c320e0ae49d733158584240bf80eedf89212975
ajithittan <ajithittan@gmail.com>
2024-05-04 12:15:28 -0400

    ok
    ok

--- server/externalsites/yahooquotes.js
+++ server/externalsites/yahooquotes.js
@@ -6,7 +6,7 @@ const getQuotesWithDates = async (stksym,frmdate,enddt) => {
           period1: frmdate,
           period2: enddt
         }).then(result => response=result)
-        response = response.map(obj => ({ ...obj, symbol: stksym}))  
+        response = response.map(obj => ({ ...obj,date:obj.date.toISOString().split('T')[0], symbol: stksym}))  
     }catch(err){
         console.log("error in Yahoo function getQuotesWithDates",err)
     }
